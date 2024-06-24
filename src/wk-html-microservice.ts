44ac904aec81fb965b7c73ec5779df2f7bdee5e6
Daniel Rodriguez <drodriguez@picap.co>
2024-06-24 14:32:59 -0500

    removed log

--- src/wk-html-microservice.ts
+++ src/wk-html-microservice.ts
@@ -55,7 +55,7 @@ export class WkHtmlMicroservice {
       this.expressApp.post('/pdf', (request: Request, response: Response) => {
         console.log("--/pdf")
          response.writeHead(200, { 'Content-Type': 'application/pdf' });
-         this.wkHtmlConverter.pdf(strtostr(request.body.html), request.body.wkConfig).pipe(process.stdout);
+         //this.wkHtmlConverter.pdf(strtostr(request.body.html), request.body.wkConfig).pipe(process.stdout);
          this.wkHtmlConverter.pdf(strtostr(request.body.html), request.body.wkConfig).pipe(response);
       });
 
